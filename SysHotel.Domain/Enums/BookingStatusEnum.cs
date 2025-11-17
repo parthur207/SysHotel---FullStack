990965ee62cc6ccb25fd7c54b5856df59b7d2101
Paulo Andrade <parthur207@gmail.com>
2025-11-17 18:33:42 -0300

    Criação dos enumeadores das reservas e criação dos enumeradores dos status de response pattern.

---
+++ SysHotel.Domain/Enums/BookingStatusEnum.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SysHotel.Domain.Enums
+{
+    public enum BookingStatusEnum
+    {
+        PendingPayment=1,
+        Reservaded = 2,
+        Canceled=3,
+        Attended= 4,
+        NoShow=5,
+    }
+}
